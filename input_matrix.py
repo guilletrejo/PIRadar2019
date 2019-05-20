dc9d20e9dbb457d9b21622af97f7a19da6e156df
guilletrejo <guille.trejo14@gmail.com>
2019-05-20 12:40:09 -0300

    Ver si se puede paralelizar input matrix py

--- input_matrix.py
+++ input_matrix.py
@@ -8,6 +8,8 @@ import netCDF4
 import wrf 
 import sys
 import datetime
+import progressbar as pb
+from joblib import Parallel, delayed
 #np.set_printoptions(threshold=sys.maxsize) # Para que las matrices se impriman completas y no resumidas
 
 '''
@@ -17,6 +19,7 @@ path_datos = "/home/datos/wrf/wrfout/"  # en YAKU, cambiar esto
 time_init = 6                    # Se ignoran las primeras 6 horas de cada archivo.
 times = 12                       # Se toman 12 horas de cada archivo. (SON 2 ARCHIVOS POR DIA)
 horas = ["06:00:00","18:00:00"]
+nombres = ["wrfout_d01_{}_{}","wrfout_A_d01_{}_{}","wrfout_B_d01_{}_{}","wrfout_C_d01_{}_{}","wrfout_D_d01_{}_{}"]
 index_horas = 1                  # Se empieza desde las 18hs del primer dia, para coordinar con las iteraciones siguientes
 alturas = range(35)         
 base = datetime.date(2017,10,31)  # Primer dia del dataset (SIEMPRE LOS DATOS VAN A INICIAR DESDE EL DIA SIGUIENTE A ESTE DIA INICIAL, DEBIDO
@@ -28,11 +31,10 @@ date_list = [base + datetime.timedelta(days=x) for x in range(1, numdays)]  # Ge
 dias = []
 for x in range (0,numdays-1):
     dias.append(date_list[x].strftime("%Y-%m-%d"))
- 
 """
 Abrir el dataset como una matriz XARRAY, dando formato al nombre con la listas de fechas y las 2 posibles horas de inicio.
 """
-dataDIR = path_datos + "{}/wrfout_A_d01_{}_{}".format(base.strftime("%Y-%m-%d"),horas[index_horas])
+dataDIR = path_datos + "{}/wrfout_A_d01_{}_{}".format(base.strftime("%Y_%m"),base.strftime("%Y-%m-%d"),horas[index_horas])
 DS = xarray.open_dataset(dataDIR)
 index_horas ^= 1                 # Se swapea para abrir el proximo archivo del mismo dia con la otra hora (6hs)
 
@@ -51,7 +53,6 @@ Cada matriz de datos tiene dimension (86,135) (rectangulo formado por las estaci
        todos los dias del dataset en CADA UNA de las 34 alturas posibles del modelo, por lo que las dimensiones de la matriz
        final serian (cant_de_horas,86,135), y se genera UN ARCHIVO (.npy) por cada altura del modelo.
 La cantidad de horas obtenidas es (numdays*24horas)-12horas
-
 """
 
 for h in alturas:                                                          # Alturas en las que se van a generar los archivos (SOLO PARA EL PRIMER DIA)
@@ -72,25 +73,38 @@ for h in alturas:                                                          # Alt
     
 DS.close() # Se cierra el dataset abierto para ahorrar memoria
 
-for dia in dias:                                                           # Lee el resto de los dias del dataset
+for dia in pb.progressbar(dias):                                                           # Lee el resto de los dias del dataset
     for hora in horas:                                                     # Lee ambas horas de cada dia (6hs y 18hs)
-        dataDIR = path_datos + "wrfout_A_d01_{}_{}".format(dia,hora) 
-        DS = xarray.open_dataset(dataDIR)                                  # Abre el archivo
+        data_found = 0
+        for nombre in nombres:
+            year = dia.split("-")[0]                                       # Obtiene anio y mes para acceder con el formato
+            month = dia.split("-")[1]                                      # de fecha de las carpetas de yaku
+            dataDIR = path_datos + "{}_{}/".format(year,month) + nombre.format(dia,hora) 
+            try:
+                DS = xarray.open_dataset(dataDIR)
+                data_found = 1
+                break
+            except IOError as e:
+                print (e.filename)
 
         for h in alturas: 
-            # Inicializacion con el tiempo 0
-            PH_numpy = DS.PH.sel(Time = time_init, bottom_top_stag = h  ).values[70:156,73:208]
-            PHB_numpy = DS.PHB.sel(Time = time_init, bottom_top_stag = h ).values[70:156,73:208]
-            z = (PH_numpy + PHB_numpy) / 9.81
-            z_ex = np.expand_dims(z, axis = 0)
+            if(data_found == 0):
+                z_ex = np.zeros(shape=(12,86,135))
+                z_ex.fill(np.nan)
+            elif (data_found == 1):
+                # Inicializacion con el tiempo 0
+                PH_numpy = DS.PH.sel(Time = time_init, bottom_top_stag = h  ).values[70:156,73:208]
+                PHB_numpy = DS.PHB.sel(Time = time_init, bottom_top_stag = h ).values[70:156,73:208]
+                z = (PH_numpy + PHB_numpy) / 9.81
+                z_ex = np.expand_dims(z, axis = 0)
 
-            # Completa con el resto de los tiempos
-            for t in range(time_init+1,time_init+times):
-                PH_numpy = DS.PH.sel(Time = t, bottom_top_stag = h ).values[70:156,73:208]
-                PHB_numpy = DS.PHB.sel(Time = t, bottom_top_stag = h ).values[70:156,73:208]
-                zaux = (PH_numpy + PHB_numpy) / 9.81
-                zaux_ex = np.expand_dims(zaux, axis = 0)
-                z_ex = np.concatenate((z_ex, zaux_ex), axis=0)
+              # Completa con el resto de los tiempos
+                for t in range(time_init+1,time_init+times):
+                    PH_numpy = DS.PH.sel(Time = t, bottom_top_stag = h ).values[70:156,73:208]
+                    PHB_numpy = DS.PHB.sel(Time = t, bottom_top_stag = h ).values[70:156,73:208]
+                    zaux = (PH_numpy + PHB_numpy) / 9.81
+                    zaux_ex = np.expand_dims(zaux, axis = 0)
+                    z_ex = np.concatenate((z_ex, zaux_ex), axis=0)
             z_file = np.load('./datos_modelo/z_altura{}_{}.npy'.format(h,dias[0]))  # Abre la matriz .npy generada anteriormente
             z_ex = np.concatenate((z_file, z_ex), axis=0)            # Concatena la nueva matriz con la anterior
             np.save('./datos_modelo/z_altura{}_{}.npy'.format(h,dias[0]),z_ex)      # Guarda nuevamente el archivo .npy (se va sobreescribiendo el archivo con los nuevos valores)
