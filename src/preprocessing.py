a165981bed38cb363b99e721bb623c4505e687fd
guilletrejo <guille.trejo14@gmail.com>
2019-07-16 14:45:49 +0000

    SMOTE

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,6 +1,7 @@
 
 import numpy as np
 import progressbar as pb
+from imblearn.over_sampling import SMOTE
 '''
 	Parametros
 '''
@@ -17,17 +18,29 @@ missing = np.where(y[:,estacion]==-1)
 	Eliminacion de -1 y seleccion de una sola estacion
 '''
 y1 = np.delete(y,missing,0)
-y = y1[:,53]
-y = np.expand_dims(y,axis=1) ## Ver si hace falta hacer esta expansion
+Y = y1[:,estacion]
+#y = np.expand_dims(y,axis=1) ## Ver si hace falta hacer esta expansion
 
+'''
+	Concatena varias alturas
+'''
 x = np.ndarray(shape=(13000,96,144,1))
 for h in pb.progressbar(alturas):
 	'''
 	Carga de datos
 	'''
 	X = np.delete(np.load(X_data_dir.format(h)),missing,0)
+	
 	'''
-		Normalizacion y estandarizacion del input
+	Oversampling
+	'''
+	# Flatten
+	X = np.reshape(X,(X.shape[0],13824))
+	sm = SMOTE(sampling_strategy='minority', random_state=7)
+	X_us, Y_us = sm.fit_sample(X,Y)
+	X = np.reshape(X_us,(X_us.shape[0],96,144))
+	'''
+	Normalizacion y estandarizacion del input
 	'''
 	u = np.mean(X)
 	s = np.std(X)
@@ -37,21 +50,38 @@ for h in pb.progressbar(alturas):
 
 	x = np.concatenate((x,X), axis = 3)
 
-
 '''
 	Quedarse con solo 2000 datos para tener 40% de lluvias
-'''
+
 indices_lluvias = np.where(y==1)[0]
 indices_nolluvias = np.where(y==0)[0][0:1250]
 Y = np.concatenate((y[indices_lluvias], y[indices_nolluvias]))
 X = np.concatenate((x[indices_lluvias], x[indices_nolluvias]))
+'''
 
-idxs = np.arange(X.shape[0])
+idxs = np.arange(x.shape[0])
 np.random.seed(0)
 np.random.shuffle(idxs)
 
-X = X[idxs, :, :, :]
+'''
+    Contar cuantos 1 hay en total en todas las estaciones.
+'''
+lluvias = np.where(Y==1)[0].size
+nolluvias = np.where(Y==0)[0].size
+missing = np.where(Y==-1)[0].size
+print("Cant. de datos lluvia: " + str(lluvias))
+print("Cant. de datos no lluvia: " + str(nolluvias))
+print("Cant. de datos faltantes: " + str(missing))
+total = lluvias+nolluvias+missing
+total_real = lluvias+nolluvias
+print("Total de datos (con faltantes): " + str(total))
+print("Total de datos (sin faltantes): " + str(total_real))
+print("Porcentaje de datos utiles: " + str(total_real/total))
+print("Porcentaje de datos lluvia: " + str(lluvias/(total_real)))
+print("Porcentaje de datos no lluvia: " + str(nolluvias/(total_real)))
+
+X = x[idxs, :, :, :]
 Y = Y[idxs]
 
-np.save("/home/awf/datos_modelo/X_6alt_iter_scaled.npy", X)
-np.save("/home/awf/datos_lluvia/Y_6alt_iter_scaled.npy", Y)
+np.save("/home/awf/datos_modelo/X_6alt_iter_scaled_smote.npy", X)
+np.save("/home/awf/datos_lluvia/Y_6alt_iter_scaled_smote.npy", Y)
