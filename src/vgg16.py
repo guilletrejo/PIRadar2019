fb54f0d6b5505d0568334788cb7453b2b55e35e7
guilletrejo <guille.trejo14@gmail.com>
2019-07-17 22:48:30 +0000

    Masked loss

--- src/vgg16.py
+++ src/vgg16.py
@@ -1,3 +1,4 @@
+from keras import backend as K
 from keras.models import Sequential
 from keras import metrics
 from keras.utils import to_categorical
@@ -6,6 +7,7 @@ from keras.layers import BatchNormalization, Conv2D, UpSampling2D, MaxPooling2D,
 from keras.optimizers import Adam, SGD
 from keras import regularizers
 from keras.callbacks import LearningRateScheduler
+import tensorflow as tf
 import numpy as np
 
 '''
@@ -14,8 +16,8 @@ import numpy as np
 muestras_train = 10400
 muestras_test = 2600
 shape = (96,144,3) # grilla de 96x144 con 3 canales
-X_data_dir = "/home/lac/datos_modelo/X_9alt_scaled.npy"
-Y_data_dir = "/home/lac/datos_lluvia/precipitacion.npy"
+X_data_dir = "/home/lac/datos_modelo/Xallest.npy"
+Y_data_dir = "/home/lac/datos_lluvia/Yallest.npy"
 model_dir = "/home/lac/modelos/modeloVgg122est3alt.h5"
 mask_dir = "/home/lac/datos_lluvia/mask_precipitacion.npy"
 cant_epocas = 20
@@ -23,8 +25,8 @@ tam_batch = 50 # intentar que sea multiplo de la cantidad de muestras
 '''
     Carga de datos
 '''
-X = np.load(X_data_dir)
-Y = np.load(Y_data_dir)
+X = np.load(X_data_dir)[:13000]
+Y = np.load(Y_data_dir)[:13000]
 print(X.shape)
 print(Y.shape)
 y_train = Y[:muestras_train]
@@ -36,9 +38,33 @@ x_test = X[muestras_train:muestras_train+muestras_test]
     Definicion del modelo y custom loss function
 '''
 
-def masked_binary_crossentropy(y_true, y_pred):
-    M = np.load(mask_dir)
-    return M*K.mean(K.binary_crossentropy(y_true, y_pred), axis=-1)
+def custom_binary_crossentropy(y_true, y_pred):
+    return K.mean(K.binary_crossentropy(tf.multiply(y_pred, tf.cast(tf.not_equal(y_true, -1), tf.float32)),
+                                        tf.multiply(y_true, tf.cast(tf.not_equal(y_true, -1), tf.float32))), axis=-1)
+
+
+def custom_binary_accuracy(y_true, y_pred):
+    t0 = tf.equal(y_true, 0)
+    t1 = tf.equal(y_true, 1)
+    p0 = tf.equal(tf.round(y_pred), 0)
+    p1 = tf.equal(tf.round(y_pred), 1)
+    everything = tf.reduce_sum(tf.cast(t0, tf.int32)) + tf.reduce_sum(tf.cast(t1, tf.int32))
+    positives = tf.reduce_sum(tf.cast(tf.logical_and(t0, p0), tf.int32)) + tf.reduce_sum(tf.cast(tf.logical_and(p1, t1), tf.int32))
+    return positives / everything
+
+
+def precision(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(tf.multiply(y_true, tf.cast(tf.not_equal(y_true, -1), tf.float32)) * tf.multiply(y_pred, tf.cast(tf.not_equal(y_true, -1), tf.float32)), 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(tf.multiply(y_pred, tf.cast(tf.not_equal(y_true, -1), tf.float32)), 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    return precision
+
+
+def recall(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(tf.multiply(y_true, tf.cast(tf.not_equal(y_true, -1), tf.float32)) * tf.multiply(y_pred, tf.cast(tf.not_equal(y_true, -1), tf.float32)), 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(tf.multiply(y_true, tf.cast(tf.not_equal(y_true, -1), tf.float32)), 0, 1)))
+    recall = true_positives / (possible_positives + K.epsilon())
+    return recall
 
 def get_vgg16():
     # we initialize the model
@@ -91,11 +117,11 @@ def get_vgg16():
     model.add(Dense(4096, activation='relu'))
     #model.add(BatchNormalization(axis=3))
     model.add(Dense(4096, activation='relu'))
-    model.add(Dense(1, activation='sigmoid'))
+    model.add(Dense(122, activation='sigmoid'))
 
     #adam = Adam(lr=0.001)
     sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-    model.compile(loss=masked_binary_crossentropy, optimizer=sgd, metrics=[metrics.binary_accuracy])
+    model.compile(loss=custom_binary_crossentropy, optimizer=sgd, metrics=[custom_binary_accuracy,precision])
 #    print(model.summary())
 
     return model
